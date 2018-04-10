ccf8c1bc29734f8a0bcaafee9d138ee1e965009d
Rhianne Price <price.rhianne@gmail.com>
2018-04-11 11:54:56 +1200

    ass model for order item and set up associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_10_232532) do
+ActiveRecord::Schema.define(version: 2018_04_10_234228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
+
+  create_table "order_items", force: :cascade do |t|
+    t.string "menu_item_id"
+    t.integer "quantity"
+    t.bigint "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.uuid "uuid", default: -> { "uuid_generate_v4()" }
