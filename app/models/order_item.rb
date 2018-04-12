3e24b264246d27c7b44c3f64011bd47b37dcc278
Rhianne Price <price.rhianne@gmail.com>
2018-04-12 14:04:24 +1200

    add validation for stock level

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,19 @@ class OrderItem < ApplicationRecord
 
   validates :quantity, presence: true
   :menu_item_id
+  validate :quantity_cannot_be_greater_than_stock_level
+
+  def quantity_cannot_be_greater_than_stock_level
+    if quantity.present? && quantity > current_stock_level
+      errors.add(:quantity, "you ordered #{quantity}x #{name} but there are only #{current_stock_level} left")
+    end
+  end
+
+  def current_stock_level
+    MenuItemFetcher.stock_level(menu_item_id)
+  end
+
+  def name
+    MenuItemFetcher.name(menu_item_id)
+  end
 end
--- app/services/menu_item_fetcher.rb
+++ app/services/menu_item_fetcher.rb
@@ -5,4 +5,13 @@ module MenuItemFetcher
   def self.menu_items
     self.get("/menu_items").parsed_response["menu_items"]
   end
+
+  def self.stock_level_for(menu_item_id)
+    self.get("/menu_items/#{menu_item_id}").parsed_response["stock_level"]
+  end
+
+  def self.name_for(menu_item_id)
+    self.get("/menu_items/#{menu_item_id}").parsed_response["name"]
+  end
+
 end
